869dc6ca0dc8fbe5d4c40e9c7b781f719acfa7b2
anshprat <anshprat@gmail.com>
2014-10-05 09:26:50 +0000

    updated echo msg to mpstat from vmstat in mpstat.sh

--- metric.d/mpstat.sh
+++ metric.d/mpstat.sh
@@ -4,7 +4,7 @@ CONFIG_FILE=$1
 . $CONFIG_FILE
 HOSTNAME=$(hostname -s)
 
-echo "Launching vmstat monitoring, reporting data to $graphite_host"
+echo "Launching mpstat monitoring, reporting data to $graphite_host"
 mpstat -P ALL $graphite_interval_seconds | gawk -f $QUICKSTATD_HOME/awk/mpstat.awk hostname=$HOSTNAME graphite_host=$graphite_host graphite_port=$graphite_port &
 echo $! >> $PID_FILE
  
